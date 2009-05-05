6b931d2cffc3c713576fad619e5e79dbbea51dbf
Theo Cushion <theo@jivatechnology.com>
2009-05-05 15:59:35 +0100

    Fix: References Rubystats correctly now

--- lib/tally/tallyable.rb
+++ lib/tally/tallyable.rb
@@ -30,7 +30,7 @@ module Tally
         if n == 0
           return 0
         end
-        z = Rubystats::NormalDistribution.new.icdf(1-power/2)
+        z = ::Rubystats::NormalDistribution.new.icdf(1-power/2)
         phat = 1.0*pos/n
         (phat + z*z/(2*n) - z * Math.sqrt((phat*(1-phat)+z*z/(4*n))/n))/(1+z*z/n)
       end    
